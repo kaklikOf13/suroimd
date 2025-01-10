a41d2d8c6004bf2ad59aa8422a88a246d0402e49
kaklikOf13 <canaldokaklik@gmail.com>
2025-01-10 19:03:27 -0300

    fix:gas_out_after_win

--- server/src/game.ts
+++ server/src/game.ts
@@ -456,7 +456,7 @@ export class Game implements GameData {
         ) {
             this.pluginManager.emit("game_end", this);
 
-            const ct=Math.max(this.gas.currentRadius>0?2000/this.gas.currentRadius:this.gas.mapSize/100,3)
+            const ct=Math.max(this.gas.currentRadius>0?(this.gas.mapSize*5.5)/this.gas.currentRadius:this.gas.mapSize/100,3)
 
             this.gas.clearGas(ct);
             const sd=Math.floor(ct*1000)-500
